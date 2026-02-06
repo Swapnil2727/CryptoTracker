3280f996c172624881f7fe6470a9d6ed1c86a17f
Swapnil2727 <sapatel2727@gmail.com>
2026-02-07 10:33:18 +1100

    Create Empty modules and define relationship - add koin to version catalogue

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,5 @@
+enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
+
 pluginManagement {
     repositories {
         google {
@@ -21,4 +23,6 @@ dependencyResolutionManagement {
 
 rootProject.name = "CryptoTracker"
 include(":app")
- 
+include(":data")
+include(":domain")
+include(":presentation")
